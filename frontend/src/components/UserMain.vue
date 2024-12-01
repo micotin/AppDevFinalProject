1a68e8612cabd5dc11e05ba22c811a645731d520
manlangitfrancis <manlangitfrancis63@gmail.com>
2024-12-01 22:28:13 +0800

    initial commit

--- frontend/src/components/UserMain.vue
+++ frontend/src/components/UserMain.vue
@@ -5,7 +5,7 @@
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
           <!-- Brand -->
-          <router-link class="navbar-brand" to="/">
+          <router-link class="navbar-brand" to="">
             <span class="brand-text">SAM1 Flower Shop</span>
           </router-link>
   
